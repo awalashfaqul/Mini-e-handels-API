6ae89d365bef8907cbbc3e025af2b76bfca585ce
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-08-28 20:16:24 +0200

    Interface for category repository. This interface defines the methods for managing categories in the e-commerce API

---
+++ Data/Repositories/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mini_e_handels_API.Models;
+
+namespace Mini_e_handels_API.Data.Repositories.IRepositories
+{
+    public interface ICategoryRepository
+    {
+        // Interface for category repository
+        // This interface defines the methods for managing categories in the e-commerce API
+        IEnumerable<Category> GetAllCategories(); // Retrieve all categories
+        Category GetCategoryById(int id); // Retrieve a category by its ID
+        void AddCategory(Category category); // Add a new category
+        //void UpdateCategory(Category category); // Update an existing category
+        void DeleteCategory(int id); // Delete a category by its ID
+    }
+}
