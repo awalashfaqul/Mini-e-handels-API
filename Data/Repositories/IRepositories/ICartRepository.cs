c3e01ac1c18eadf0c25b617995e695ab59651f75
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-08-29 23:33:41 +0200

    Interface updated; added CreateCart; commented UpdateCart.

--- Data/Repositories/IRepositories/ICartRepository.cs
+++ Data/Repositories/IRepositories/ICartRepository.cs
@@ -11,9 +11,10 @@ namespace Mini_e_handels_API.Data.Repositories.IRepositories
         // Interface for cart repository
         // This interface defines the methods for managing shopping carts in the e-commerce API
         ShoppingCart GetAllCarts(); // Retrieve all shopping carts
+        ShoppingCart CreateCart(); // Create a new shopping cart
         ShoppingCart GetCartById(int id); // Retrieve a shopping cart by its ID
         void AddItem(int cartId, ShoppingCartItem item); // Add a new item to the cart
-        void UpdateCart(ShoppingCart cart); // Update an existing shopping cart
+        //void UpdateCart(ShoppingCart cart); // Update an existing shopping cart
         void RemoveItem(int cartId, int productId); // Remove an item from the cart
         void ClearCart(int id); // Clear all items from a shopping cart by its ID
     }
