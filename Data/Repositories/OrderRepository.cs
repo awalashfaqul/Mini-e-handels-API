f8823a7b2d7883195bf339efcad870159ed69cca
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-08-29 23:38:16 +0200

    Order Repository created

---
+++ Data/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mini_e_handels_API.Models;
+using Mini_e_handels_API.Data.Repositories.IRepositories;
+
+namespace Mini_e_handels_API.Data.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly List<ShoppingOrder> _orders = new();
+
+        public ShoppingOrder CreateOrder(ShoppingCart cart)
+        {
+            var newOrder = new ShoppingOrder
+            {
+                Id = _orders.Count + 1,
+                OrderItems = cart.CartItems,
+                TotalAmount = cart.TotalPrice
+            };
+            _orders.Add(newOrder);
+            return newOrder;
+        }
+
+        public IEnumerable<ShoppingOrder> GetAll()
+        {
+            return _orders;
+        }
+
+        public ShoppingOrder GetById(int id)
+        {
+            return _orders.FirstOrDefault(o => o.Id == id);
+        }
+    }
+}
