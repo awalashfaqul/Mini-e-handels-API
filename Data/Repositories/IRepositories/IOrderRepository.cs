6b4994be6521c02f570cd7308d7aa965e1d4922b
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-08-29 23:00:22 +0200

    Interface for shopping order

---
+++ Data/Repositories/IRepositories/IOrderRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mini_e_handels_API.Models;
+
+namespace Mini_e_handels_API.Data.Repositories.IRepositories
+{
+    public interface IOrderRepository
+    {
+        ShoppingOrder CreateOrder(ShoppingCart cart);
+        IEnumerable<ShoppingOrder> GetAll();
+        ShoppingOrder GetById(int id);
+    }
+}
