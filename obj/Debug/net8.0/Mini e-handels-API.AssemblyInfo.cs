dd5de1c27550dfb7f80514b6f0a0498b58642aa9
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-08-30 00:18:44 +0200

    system update

--- obj/Debug/net8.0/Mini e-handels-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mini e-handels-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini e-handels-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b753333162dee3e44bddc915e7e89e3a234922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f093b0efc132a5f1176c6c7420679931128f65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini e-handels-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini e-handels-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
