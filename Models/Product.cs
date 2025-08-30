604d37d132ac5d4dd86f41d63055c53c7303fa7e
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-08-30 17:30:38 +0200

    commented out category nme from Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -10,8 +10,7 @@ namespace Mini_e_handels_API.Models
         public int Id { get; set; }            // Unique identifier for the product
         public string Name { get; set; }        // Name of the product
         public string Description { get; set; } // Description of the product
-        
-        public string Category { get; set; }   // Category of the product
+        //public string Category { get; set; }   // Category of the product
         public int Price { get; set; }          // Price of the product
         public int CategoryId { get; set; }     // Foreign key for the category
     }
