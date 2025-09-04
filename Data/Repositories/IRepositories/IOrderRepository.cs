96022e8fc89ef42d308e1d93d42e932622e7809c
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-09-04 22:36:10 +0200

    Order repo interface updated

--- Data/Repositories/IRepositories/IOrderRepository.cs
+++ Data/Repositories/IRepositories/IOrderRepository.cs
@@ -8,11 +8,13 @@ namespace Mini_e_handels_API.Data.Repositories.IRepositories
 {
     public interface IOrderRepository
     {
-        ShoppingOrder CreateOrder(ShoppingCart cart);
+        ShoppingOrder CreateOrder(ShoppingCart cart, Customer? customer = null); // Modified to accept an optional Customer parameter
         IEnumerable<ShoppingOrder> GetAll();
         ShoppingOrder GetById(int id);
         void Create(ShoppingOrder order);
         void Update(ShoppingOrder order);
         void Delete(int id);
+        IEnumerable<ShoppingOrder> GetByCustomerId(int customerId); // New method to get orders by customer ID. This will make tracking customer order history easier.
+
     }
 }
